3c6dc20c32f374eaaf3bbd311f379760b928539c
yuki203767r <74959058+yuki203767r@users.noreply.github.com>
2022-07-25 23:41:11 +0800

    pop up inforgraphics at main activity + intent to educational activity

--- ecovision/app/src/main/java/com/google/codelabs/buildyourfirstmap/EducationalVideos.kt
+++ ecovision/app/src/main/java/com/google/codelabs/buildyourfirstmap/EducationalVideos.kt
@@ -11,12 +11,22 @@ import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import androidx.annotation.NonNull
 
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
-
-
-
+import com.smarteist.autoimageslider.SliderView
 
 
 class EducationalVideos : AppCompatActivity() {
+    // on below line we are creating a variable
+    // for our array list for storing our images.
+    lateinit var imageUrl: ArrayList<String>
+
+    // on below line we are creating
+    // a variable for our slider view.
+    lateinit var sliderView: SliderView
+
+    // on below line we are creating
+    // a variable for our slider adapter.
+    lateinit var sliderAdapter: SliderAdapter
+
     private lateinit var binding: ActivityEducationalVideosBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,5 +37,35 @@ class EducationalVideos : AppCompatActivity() {
         val youTubePlayerView = findViewById<YouTubePlayerView>(binding.youtubePlayerView.id)
         lifecycle.addObserver(youTubePlayerView)
 
+        // on below line we are initializing our slier view.
+        sliderView = findViewById(com.google.codelabs.buildyourfirstmap.R.id.aslider)
+
+        // on below line we are initializing
+        // our image url array list.
+        var image = intArrayOf(com.google.codelabs.buildyourfirstmap.R.drawable.info1, com.google.codelabs.buildyourfirstmap.R.drawable.info2)
+        // on below line we are adding data to our image url array list.
+
+        // on below line we are initializing our
+        // slider adapter and adding our list to it.
+        sliderAdapter = SliderAdapter(image )
+
+        // on below line we are setting auto cycle direction
+        // for our slider view from left to right.
+        sliderView.autoCycleDirection = SliderView.LAYOUT_DIRECTION_LTR
+
+        // on below line we are setting adapter for our slider.
+        sliderView.setSliderAdapter(sliderAdapter)
+
+        // on below line we are setting scroll time
+        // in seconds for our slider view.
+        sliderView.scrollTimeInSec = 10
+
+        // on below line we are setting auto cycle
+        // to true to auto slide our items.
+        sliderView.isAutoCycle = true
+
+        // on below line we are calling start
+        // auto cycle to start our cycle.
+        sliderView.startAutoCycle()
     }
 }
--- ecovision/app/src/main/java/com/google/codelabs/buildyourfirstmap/MainActivity.kt
+++ ecovision/app/src/main/java/com/google/codelabs/buildyourfirstmap/MainActivity.kt
@@ -16,9 +16,11 @@ package com.google.codelabs.buildyourfirstmap
 
 import android.annotation.SuppressLint
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
+import android.widget.PopupWindow
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
@@ -42,222 +44,106 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.json.JSONObject
 
-private val mGeoApiContext: GeoApiContext? = null
+import android.view.LayoutInflater
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.transition.Slide
+import android.transition.TransitionManager
+import android.view.Gravity
+import android.widget.*
+import android.os.Handler;
+import android.os.Looper
 
-lateinit var mapFragment : SupportMapFragment
-lateinit var mMap: GoogleMap
 class MainActivity : AppCompatActivity() {
 
-    private val places: List<Place> by lazy {
-        PlacesReader(this).read()
-    }
 
 
     // [START maps_android_add_map_codelab_ktx_coroutines]
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val mapFragment =
-            supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
-        lifecycleScope.launchWhenCreated {
-            // Get map
-            mMap = mapFragment.awaitMap()
 
-            addClusteredMarkers(mMap)
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+//        button.setOnClickListener{
+//             Initialize a new layout inflater instance
+            val inflater:LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
-            // Wait for map to finish loading
-            mMap.awaitMapLoad()
+            // Inflate a custom view using layout inflater
+            val view = inflater.inflate(R.layout.popup,null)
 
-            // Ensure all places are visible in the map
-            val bounds = LatLngBounds.builder()
-            places.forEach { bounds.include(it.latLng) }
+            // Initialize a new instance of popup window
+            val popupWindow = PopupWindow(
+                view, // Custom view to show in popup window
+                LinearLayout.LayoutParams.WRAP_CONTENT, // Width of popup window
+                LinearLayout.LayoutParams.WRAP_CONTENT // Window height
+            )
 
-            val currentLocation = LatLng(1.3695,103.8484)
+            // Set an elevation for the popup window
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                popupWindow.elevation = 10.0F
+            }
 
-            val recycleBinLocationMock = LatLng(1.369215,103.846436)
-            mMap.addMarker(MarkerOptions().position(currentLocation).title("You"))
-            //Add marker for recycle bin.
-            mMap.addMarker(MarkerOptions().position(recycleBinLocationMock).title("RecycleBin 1").icon(binIcon))
 
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation,17f))
+            // If API level 23 or higher then execute the code
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                // Create a new slide animation for popup window enter transition
+                val slideIn = Slide()
+                slideIn.slideEdge = Gravity.TOP
+                popupWindow.enterTransition = slideIn
 
-            Log.d("GoogleMap", "before URL")
-            val URL = getDirectionURL(currentLocation,recycleBinLocationMock)
-            Log.d("GoogleMap", "URL : $URL")
-            GetDirection(URL).execute()
-        }
+                // Slide animation for popup window exit transition
+                val slideOut = Slide()
+                slideOut.slideEdge = Gravity.RIGHT
+                popupWindow.exitTransition = slideOut
 
+            }
+
+            // Get the widgets reference from custom view
+//            val tv = view.findViewById<TextView>(R.id.text_view)
+            val buttonPopup = view.findViewById<ImageView>(R.id.cross)
+            val buttonLearn = view.findViewById<Button>(R.id.buttonlearn)
+
+//            // Set click listener for popup window's text view
+//            tv.setOnClickListener{
+//                // Change the text color of popup window's text view
+//                tv.setTextColor(Color.RED)
+//            }
+//
+//            // Set a click listener for popup's button widget
+            buttonPopup.setOnClickListener{
+                // Dismiss the popup window
+                popupWindow.dismiss()
+            }
+
+            // Set a dismiss listener for popup window
+            popupWindow.setOnDismissListener {
+                Toast.makeText(applicationContext,"Popup closed",Toast.LENGTH_SHORT).show()
+            }
+
+            buttonLearn.setOnClickListener{
+                val intent = Intent(this,EducationalVideos::class.java)
+                startActivity(intent)
+            }
+
+
+            // Finally, show the popup window on app
+            TransitionManager.beginDelayedTransition(root_layout)
+            popupWindow.showAtLocation(
+                root_layout, // Location to display popup window
+                Gravity.CENTER, // Exact position of layout to display popup
+                0, // X offset
+                0 // Y offset
+            )
+        }, 50)
 
     }
+    }
+//    }
     // [END maps_android_add_map_codelab_ktx_coroutines]
 
 
-    /**
-     * Adds markers to the map with clustering support.
-     */
-    private fun addClusteredMarkers(googleMap: GoogleMap) {
-        // Create the ClusterManager class and set the custom renderer
-        val clusterManager = ClusterManager<Place>(this, googleMap)
-        clusterManager.renderer =
-            PlaceRenderer(
-                this,
-                googleMap,
-                clusterManager
-            )
 
-        // Set custom info window adapter
-        clusterManager.markerCollection.setInfoWindowAdapter(MarkerInfoWindowAdapter(this))
 
-        // Add the places to the ClusterManager
-        clusterManager.addItems(places)
-        clusterManager.cluster()
-
-        // Show polygon
-        clusterManager.setOnClusterItemClickListener { item ->
-            addCircle(googleMap, item)
-            return@setOnClusterItemClickListener false
-        }
-
-        // When the camera starts moving, change the alpha value of the marker to translucent
-        googleMap.setOnCameraMoveStartedListener {
-            clusterManager.markerCollection.markers.forEach { it.alpha = 0.3f }
-            clusterManager.clusterMarkerCollection.markers.forEach { it.alpha = 0.3f }
-        }
-
-        googleMap.setOnCameraIdleListener {
-            // When the camera stops moving, change the alpha value back to opaque
-            clusterManager.markerCollection.markers.forEach { it.alpha = 1.0f }
-            clusterManager.clusterMarkerCollection.markers.forEach { it.alpha = 1.0f }
-
-            // Call clusterManager.onCameraIdle() when the camera stops moving so that re-clustering
-            // can be performed when the camera stops moving
-            clusterManager.onCameraIdle()
-        }
-    }
-
-    private var circle: Circle? = null
-
-    // [START maps_android_add_map_codelab_ktx_add_circle]
-    /**
-     * Adds a [Circle] around the provided [item]
-     */
-    private fun addCircle(googleMap: GoogleMap, item: Place) {
-        circle?.remove()
-        circle = googleMap.addCircle {
-            center(item.latLng)
-            radius(1000.0)
-            fillColor(ContextCompat.getColor(this@MainActivity, R.color.colorPrimaryTranslucent))
-            strokeColor(ContextCompat.getColor(this@MainActivity, R.color.colorPrimary))
-        }
-    }
-    // [END maps_android_add_map_codelab_ktx_add_circle]
-
-    private val binIcon: BitmapDescriptor by lazy {
-        val color = ContextCompat.getColor(this, R.color.colorGreen)
-        BitmapHelper.vectorToBitmap(this, R.drawable.ic_recycle_bin1, color)
-    }
-
-    // [START maps_android_add_map_codelab_ktx_add_markers]
-    /**
-     * Adds markers to the map. These markers won't be clustered.
-     */
-    private fun addMarkers(googleMap: GoogleMap) {
-        places.forEach { place ->
-            val marker = googleMap.addMarker {
-                title(place.name)
-                position(place.latLng)
-                icon(binIcon)
-            }
-            // Set place as the tag on the marker object so it can be referenced within
-            // MarkerInfoWindowAdapter
-            marker?.tag = place
-        }
-    }
-    //Route Code
-    fun getDirectionURL(origin:LatLng,dest:LatLng) : String{
-        return "https://maps.googleapis.com/maps/api/directions/json?origin=${origin.latitude},${origin.longitude}&destination=${dest.latitude},${dest.longitude}&sensor=false&mode=driving&key=${GOOGLE_MAPS_API_KEY}"
-    }
-
-    private inner class GetDirection(val url : String) : AsyncTask<Void,Void,List<List<LatLng>>>(){
-        override fun doInBackground(vararg params: Void?): List<List<LatLng>> {
-            val client = OkHttpClient()
-            val request = Request.Builder().url(url).build()
-            val response = client.newCall(request).execute()
-            val data = response.body!!.string()
-            Log.d("GoogleMap" , " data : $data")
-            val result =  ArrayList<List<LatLng>>()
-            try{
-                val respObj = Gson().fromJson(data,MapData::class.java)
-
-                val path =  ArrayList<LatLng>()
-
-                for (i in 0..(respObj.routes[0].legs[0].steps.size-1)){
-//                    val startLatLng = LatLng(respObj.routes[0].legs[0].steps[i].start_location.lat.toDouble()
-//                            ,respObj.routes[0].legs[0].steps[i].start_location.lng.toDouble())
-//                    path.add(startLatLng)
-//                    val endLatLng = LatLng(respObj.routes[0].legs[0].steps[i].end_location.lat.toDouble()
-//                            ,respObj.routes[0].legs[0].steps[i].end_location.lng.toDouble())
-                    path.addAll(decodePolyline(respObj.routes[0].legs[0].steps[i].polyline.points))
-                }
-                result.add(path)
-            }catch (e:Exception){
-                e.printStackTrace()
-            }
-            return result
-        }
-
-        override fun onPostExecute(result: List<List<LatLng>>) {
-            val lineoption = PolylineOptions()
-            for (i in result.indices){
-                lineoption.addAll(result[i])
-                lineoption.width(10f)
-                lineoption.color(Color.BLUE)
-                lineoption.geodesic(true)
-            }
-            mMap!!.addPolyline(lineoption)
-        }
-    }
-
-    public fun decodePolyline(encoded: String): List<LatLng> {
-
-        val poly = ArrayList<LatLng>()
-        var index = 0
-        val len = encoded.length
-        var lat = 0
-        var lng = 0
-
-        while (index < len) {
-            var b: Int
-            var shift = 0
-            var result = 0
-            do {
-                b = encoded[index++].toInt() - 63
-                result = result or (b and 0x1f shl shift)
-                shift += 5
-            } while (b >= 0x20)
-            val dlat = if (result and 1 != 0) (result shr 1).inv() else result shr 1
-            lat += dlat
-
-            shift = 0
-            result = 0
-            do {
-                b = encoded[index++].toInt() - 63
-                result = result or (b and 0x1f shl shift)
-                shift += 5
-            } while (b >= 0x20)
-            val dlng = if (result and 1 != 0) (result shr 1).inv() else result shr 1
-            lng += dlng
-
-            val latLng = LatLng((lat.toDouble() / 1E5),(lng.toDouble() / 1E5))
-            poly.add(latLng)
-        }
-
-        return poly
-    }
-    //
-    // [END maps_android_add_map_codelab_ktx_add_markers]
-
-    companion object {
-        val TAG = MainActivity::class.java.simpleName
-    }
-}
